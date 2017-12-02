635aa33e7350860baf91fff2d1de9b3c3299c6c7
kobe Lin <kobebryant721013@gmail.com>
2017-12-03 01:10:29 +0800

    3 table cell layout done.

---
+++ MyTableViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  MyTableViewCell.swift
+//  Timetunnel
+//
+//  Created by kobe on 2017/10/7.
+//  Copyright © 2017年 kobe. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var uploadTime: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    /*
+    //將cell內縮10, 並且cell有圓角
+    override var frame: CGRect {
+        get {
+            return super.frame
+        }
+        set {
+            var frame = newValue
+            frame.origin.x += 10
+            frame.size.width -= 2 * frame.origin.x;
+            frame.size.height -= 2 * frame.origin.x;
+            
+            
+            
+            self.layer.masksToBounds = true;
+            self.layer.cornerRadius = 5.0;
+            //frame.size.width -= 2 * 25
+            
+            super.frame = frame
+        }
+    }
+ */
+}
