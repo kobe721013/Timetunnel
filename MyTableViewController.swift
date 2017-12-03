59fd57667b4e6f9dd82359dbf20615d21cdbb19d
kobe Lin <kobebryant721013@gmail.com>
2017-12-04 00:09:42 +0800

    add multiImagePicker pod, not yet done.

--- MyTableViewController.swift
+++ MyTableViewController.swift
@@ -7,8 +7,81 @@
 //
 
 import UIKit
-
-class MyTableViewController: UITableViewController {
+import Fusuma
+class MyTableViewController: UITableViewController,FusumaDelegate {
+    // MARK: FusumaDelegate Protocol
+    func fusumaImageSelected(_ image: UIImage, source: FusumaMode) {
+        
+        switch source {
+            
+        case .camera:
+            
+            print("Image captured from Camera")
+            
+        case .library:
+            
+            print("Image selected from Camera Roll")
+            
+        default:
+            
+            print("Image selected")
+        }
+        
+        imageView.image = image
+    }
+    
+    func fusumaMultipleImageSelected(_ images: [UIImage], source: FusumaMode) {
+        
+        print("Number of selection images: \(images.count)")
+        
+        var count: Double = 0
+        
+        for image in images {
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + (3.0 * count)) {
+                
+                self.imageView.image = image
+                print("w: \(image.size.width) - h: \(image.size.height)")
+            }
+            count += 1
+        }
+    }
+    
+    func fusumaImageSelected(_ image: UIImage, source: FusumaMode, metaData: ImageMetadata) {
+        
+        print("Image mediatype: \(metaData.mediaType)")
+        print("Source image size: \(metaData.pixelWidth)x\(metaData.pixelHeight)")
+        print("Creation date: \(String(describing: metaData.creationDate))")
+        print("Modification date: \(String(describing: metaData.modificationDate))")
+        print("Video duration: \(metaData.duration)")
+        print("Is favourite: \(metaData.isFavourite)")
+        print("Is hidden: \(metaData.isHidden)")
+        print("Location: \(String(describing: metaData.location))")
+    }
+    
+    func fusumaVideoCompleted(withFileURL fileURL: URL) {
+        
+        print("video completed and output to file: \(fileURL)")
+        self.fileUrlLabel.text = "file output to: \(fileURL.absoluteString)"
+    }
+    
+    func fusumaDismissedWithImage(_ image: UIImage, source: FusumaMode) {
+        
+        switch source {
+            
+        case .camera:
+            
+            print("Called just after dismissed FusumaViewController using Camera")
+            
+        case .library:
+            
+            print("Called just after dismissed FusumaViewController using Camera Roll")
+            
+        default:
+            
+            print("Called just after dismissed FusumaViewController")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Timetunnel' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+   use_frameworks!
+	 pod 'Fusuma', git: 'https://github.com/ytakzk/Fusuma.git'
+  # Pods for Timetunnel
+
+end
